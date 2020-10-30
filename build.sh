03bf03372dee6f81249b4f0965be64231f9a2e8f
dacaiguoguogmail <dacaiguoguo@gmail.com>
2020-10-30 11:20:54 +0800

    IDEBuildLocationStyle Unique

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
-/usr/bin/xcrun xcodebuild clean build -skipUnavailableActions -scheme testXcodeBuild -configuration Release -destination 'generic/platform=iOS' -workspace testXcodeBuild.xcworkspace -verbose
-
+idebuildlocationstyle=$(defaults read com.apple.dt.Xcode IDEBuildLocationStyle)
+echo $idebuildlocationstyle
+# /usr/bin/xcrun xcodebuild -showBuildSettings -json -workspace testXcodeBuild.xcworkspace -scheme testXcodeBuild  -derivedDataPath DerivedData >sett.json
+# defaults export com.apple.dt.Xcode /Users/username/Downloads/testXcodeBuild/xxx3.plist
+/usr/bin/xcrun xcodebuild clean build -IDEBuildLocationStyle='Unique' -skipUnavailableActions -workspace testXcodeBuild.xcworkspace -scheme testXcodeBuild -configuration Release -destination 'generic/platform=iOS' -derivedDataPath DerivedData  -verbose 
 # failed after add SYMROOT and OBJROOT 
 #/usr/bin/xcrun xcodebuild clean build -skipUnavailableActions -scheme testXcodeBuild -configuration Release -destination 'generic/platform=iOS' -workspace testXcodeBuild.xcworkspace -verbose SYMROOT=buildSym OBJROOT=buildObj
 
