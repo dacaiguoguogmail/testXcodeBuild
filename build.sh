9ce88eaa25b91e8afdd04a6405480853774ca3f7
dacaiguoguogmail <dacaiguoguo@gmail.com>
2020-11-13 14:17:52 +0800

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,35 @@
 idebuildlocationstyle=$(defaults read com.apple.dt.Xcode IDEBuildLocationStyle)
 echo $idebuildlocationstyle
+defaults read com.apple.dt.Xcode IDEDerivedDataPathOverride
 # /usr/bin/xcrun xcodebuild -showBuildSettings -json -workspace testXcodeBuild.xcworkspace -scheme testXcodeBuild  -derivedDataPath DerivedData >sett.json
 # defaults export com.apple.dt.Xcode /Users/username/Downloads/testXcodeBuild/xxx3.plist
-/usr/bin/xcrun xcodebuild clean build -IDEBuildLocationStyle='Unique' -skipUnavailableActions -workspace testXcodeBuild.xcworkspace -scheme testXcodeBuild -configuration Release -destination 'generic/platform=iOS' -derivedDataPath DerivedData  -verbose 
+# /usr/bin/xcrun xcodebuild clean build -IDEBuildLocationStyle='Custom' -IDECustomBuildLocationType='Absolute' -IDECustomBuildIntermediatesPath='/Users/username/Desktop/Build22/Intermediates.noindex' -IDECustomBuildProductsPath='/Users/username/Desktop/Build22/Products' -skipUnavailableActions -workspace testXcodeBuild.xcworkspace -scheme testXcodeBuild -configuration Release -destination 'generic/platform=iOS' -derivedDataPath DerivedData  -verbose 
+/usr/bin/xcrun xcodebuild clean build ONLY_ACTIVE_ARCH=YES EXCLUDED_ARCHS='i386 arm64'\
+ -skipUnavailableActions \
+ -project Pods/Pods.xcodeproj \
+ -scheme AFNetworking \
+ -configuration Release \
+ -derivedDataPath /Users/username/Downloads/testXcodeBuild/DerivedData \
+ -destination 'generic/platform=com.apple.platform.iphonesimulator' \
+ -verbose SYMROOT=buildSym OBJROOT=buildObj
+
+
+
+ /usr/bin/xcrun xcodebuild clean archive \
+ -skipUnavailableActions \
+ -project Pods/Pods.xcodeproj \
+ -scheme AFNetworking \
+ -configuration Release \
+ -derivedDataPath /Users/username/Downloads/testXcodeBuild/DerivedData \
+ -destination 'generic/platform=iOS' \
+ -verbose SYMROOT=buildSym OBJROOT=buildObj
+
+ 
+
 # failed after add SYMROOT and OBJROOT 
 #/usr/bin/xcrun xcodebuild clean build -skipUnavailableActions -scheme testXcodeBuild -configuration Release -destination 'generic/platform=iOS' -workspace testXcodeBuild.xcworkspace -verbose SYMROOT=buildSym OBJROOT=buildObj
 
-
+ # -IDEBuildLocationStyle='Custom' \
+ # -IDECustomBuildLocationType='Absolute' \
+ # -IDECustomBuildIntermediatesPath='/Users/username/Desktop/Build22/Intermediates.noindex' \
+ # -IDECustomBuildProductsPath='/Users/username/Desktop/Build22/Products' \
